b975516a5cfbb6ccb3eaf4169eb145abaea6be9f
Ponky <77840376+BaguetteConnoisseur@users.noreply.github.com>
2023-11-03 09:28:18 +0100

    Code 11-03 by Ponky

--- Cars.java
+++ Cars.java
@@ -1,43 +1,73 @@
 import java.awt.*;
 import static java.lang.System.out;
 
-class Vehicles {
-    protected int nrDoors; // Number of doors on the car
-    protected double enginePower; // Engine power of the car
-    protected double currentSpeed; // The current speed of the car
-    protected Color color; // Color of the car
-    protected String modelName; // The car model name
+class Cars {
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+    public boolean turboOn;
+    public static double trimfactor;
 
-    public void Volvo240(){
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+}
+
+class Volvo2401 extends Cars {
+    public void specs(){
         nrDoors = 4;
         color = Color.black;
         enginePower = 100;
         modelName = "Volvo240";
-        //stopEngine();
+        trimFactor = 1.25
+        super.stopEngine();
     }
+}
 
-    public void Saab95(){
+class Saab951 extends Cars {
+    public void specs(){
         nrDoors = 2;
         color = Color.red;
         enginePower = 125;
-        //turboOn = false;
+        turboOn = false;
         modelName = "Saab95";
-        //stopEngine();
+        super.stopEngine();
     }
 }
 
-
-
-
-class Cars extends Vehicles {
+class Main {
     public static void main(String[] args) {
-        Cars myCar = new Cars();
-        myCar.Saab95();
-        out.println(myCar.nrDoors);
-        myCar.Volvo240();
-        out.println(myCar.nrDoors);
+        Cars myCars = new Cars();
+        Cars mySaab95 = new Saab951();
+        Cars Volvo240 = new Volvo2401();
+        out.println(mySaab95.getNrDoors());
+        out.println(mySaab95.nrDoors);
+        out.println(Volvo240.nrDoors);
     }
-
 }
-
-
