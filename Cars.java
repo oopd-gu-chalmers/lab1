1171be40f7ef6552870121c9b80b83f6a4dcfe40
Gargis <148954493+GargisGargis@users.noreply.github.com>
2023-11-06 09:49:25 +0100

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -11,18 +11,18 @@ class Cars {
     public static double trimfactor;
 
 
-    public int getNrDoors(){
+    public final int getNrDoors(){
         return nrDoors;
     }
-    public double getEnginePower(){
+    public final double getEnginePower(){
         return enginePower;
     }
 
-    public double getCurrentSpeed(){
+    public final double getCurrentSpeed(){
         return currentSpeed;
     }
 
-    public Color getColor(){
+    public final Color getColor(){
         return color;
     }
 
@@ -48,6 +48,37 @@ class Volvo2401 extends Cars {
         trimfactor = 1.25;
         super.stopEngine();
     }
+    public void setTurboOn(){
+        turboOn = true;
+    }
+
+    public void setTurboOff(){
+        turboOn = false;
+    }
+
+    public double speedFactor(){
+        double turbo = 1;
+        if(turboOn) turbo = 1.3;
+        return enginePower * 0.01 * turbo;
+    }
+
+    public void incrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+    }
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
 }
 
 class Saab951 extends Cars {
@@ -58,6 +89,27 @@ class Saab951 extends Cars {
         turboOn = false;
         modelName = "Saab95";
         super.stopEngine();
+    }
+    public double speedFactor(){
+        return enginePower * 0.01 * trimfactor;
+    }
+
+    public void incrementSpeed(double amount){
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+    }
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
     }
 }
 
