8e5645b4800b85b28adf74ffee6b7d33c07d3d22
Evelina Frännhag <evelinafrannhag@gmail.com>
2023-11-08 11:31:11 +0100

    Assign currentSpeed default value

--- src/Car.java
+++ src/Car.java
@@ -5,7 +5,7 @@ public abstract class Car implements Movable {
     protected double[] direction = {0, 1};
     protected  int nrDoors; // Number of doors on the car
     protected double enginePower; // Engine power of the car
-    protected double currentSpeed; // The current speed of the car
+    protected double currentSpeed = 0; // The current speed of the car
     protected Color color; // Color of the car
     protected String modelName; // The car model name
 
