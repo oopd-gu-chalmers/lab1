a04016d25709ab40b5eeaa3e4f6dba4395147639
Evelina Frännhag <evelinafrannhag@gmail.com>
2023-11-08 11:50:48 +0100

    Testing of Move

--- test/testMove.java
+++ test/testMove.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class testMove {
     private Saab95 saab;
@@ -6,6 +9,19 @@ public class testMove {
 
     @Before
     public void init() {
-        
+        saab = new Saab95();
+        volvo = new Volvo240();
+    }
+
+    @Test
+    public void checkSaabStartPosition() {
+        double[] start = {0, 0};
+        assertEquals(start, saab.getPosition());
+    }
+
+    @Test
+    public void checkVolvoStartPosition() {
+        double[] start = {0, 0};
+        assertEquals(start, volvo.getPosition());
     }
 }
