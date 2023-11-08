9f3eb4d2a4f0cae80c8bc563fa8e5b8b1611e4cd
Gåsen <linda.bergevald@gmail.com>
2023-11-08 15:36:28 +0100

    changed name of file

--- test/TestSaab95StopEngine.java
+++ test/TestSaab95StopEngine.java
@@ -4,12 +4,11 @@ import src.Saab95;
 //import src.Car;
 //import static org.junit.Assert.assertTrue;
 
-public class TestStopEngineSaab95 {
+public class TestSaab95StopEngine {
     @Test
     public void StopEngineSaab95(){
         Saab95 Saab = new Saab95();
         Saab.stopEngine();
         Assert.assertEquals(0, (int)Saab.getCurrentSpeed());
     }
-    
 }
