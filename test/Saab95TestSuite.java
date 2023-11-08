892f55eccd45c58963bec4bbdacd9a0fa0dfdedd
Evelina Frännhag <evelinafrannhag@gmail.com>
2023-11-08 17:05:58 +0100

    Start testing volvo

--- test/Saab95TestSuite.java
+++
@@ -1,7 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({TestSaab95SpeedFactor.class,TestSaab95StartEngine.class})
-public class Saab95TestSuite {
-}
