def10f53c8dfd1cc08246efd4b1c29011a832e78
Alex04412 <Alex.tang20041212@gmail.com>
2023-11-08 10:33:08 +0100

    OwO

--- Saab95.java
+++ Saab95.java
@@ -1,59 +1,46 @@
 import java.awt.*;
 
-public class Saab95 extends Bilar{
+public class Saab95 extends Cars{
 
 
 
-    public boolean turboOn;
+    private boolean turboOn;
 
-    public Saab95(){
+    private Saab95(){
         super(2, 125, Color.red, "Saab95");
 	    turboOn = false;
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void setTurboOn(){
+    private void setTurboOn(){
 	    turboOn = true;
     }
 
-    public void setTurboOff(){
+    private void setTurboOff(){
 	    turboOn = false;
     }
     
-    public double speedFactor(){
+    private double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
 
-    public void incrementSpeed(double amount){
+    private void incrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() + speedFactor() * amount;
     }
 
-    public void decrementSpeed(double amount){
+    private void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
     
     // TODO fix this method according to lab pm
-    public void gas(double amount){
+    private void gas(double amount){
         incrementSpeed(amount);
     }
 
     // TODO fix this method according to lab pm
-    public void brake(double amount){
+    private void brake(double amount){
         decrementSpeed(amount);
     }
 
@@ -61,5 +48,16 @@ public class Saab95 extends Bilar{
         Saab95 car = new Saab95();
         //double speed = car.currentSpeed;
         System.out.println(car.getColor());
+        car.setTurboOn();
+        if (car.turboOn){
+            System.out.println("OwO");
+        }
+        car.startEngine();
+        car.turnLeft();
+        car.move();
+        car.turnRight();
+        car.move();
+        car.turnRight();
+        car.move();
     }
 }
--- Volvo240.java
+++ Volvo240.java
@@ -1,33 +1,33 @@
 import java.awt.*;
 
-public class Volvo240 extends Bilar{
+public class Volvo240 extends Cars{
 
-    public final static double trimFactor = 1.25;
+    private final static double trimFactor = 1.25;
 
     
-    public Volvo240(){
+    private Volvo240(){
         super(4, 100, Color.black, "Volvo240");
     }
     
-    public double speedFactor(){
+    private double speedFactor(){
         return enginePower * 0.01 * trimFactor;
     }
 
-    public void incrementSpeed(double amount){
+    private void incrementSpeed(double amount){
 	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
     }
 
-    public void decrementSpeed(double amount){
+    private void decrementSpeed(double amount){
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
     }
 
     // TODO fix this method according to lab pm
-    public void gas(double amount){
+    private void gas(double amount){
         incrementSpeed(amount);
     }
 
     // TODO fix this method according to lab pm
-    public void brake(double amount){
+    private void brake(double amount){
         decrementSpeed(amount);
     }
 }
