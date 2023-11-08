90d05ec3c2f3f6fc7bf7feb8accde4e3d9851929
Evelina Frännhag <evelinafrannhag@gmail.com>
2023-11-08 16:45:37 +0100

    Start testing volvo

--- test/TestSaab95RightTurn.java
+++ test/TestSaab95RightTurn.java
@@ -5,14 +5,71 @@ import static org.junit.Assert.assertEquals;
 
 public class TestSaab95RightTurn {
     private Saab95 saab;
+    private final double[] start = {0, 1};
+    private final double[] newDirection = {1, 0};
 
     @Before
     public void init() {
         saab = new Saab95();
-        saab.turnRight();
     }
+
     @Test
-    public void checkRightTurn() {
-        assertEquals(saab.getDirection()[0], 0);
+    public void checkInitialXDirection() {
+        assertEquals(start[0], saab.getDirection()[0], 0.01);
+    }
+
+    @Test
+    public void checkInitialYDirection() {
+        assertEquals(start[1], saab.getDirection()[1], 0.01);
+    }
+
+    @Test
+    public void checkXDirectionAfterOneRightTurn() {
+        saab.turnRight();
+        assertEquals(newDirection[0], saab.getDirection()[0], 0.01);
+    }
+
+    @Test
+    public void checkYDirectionAfterOneRightTurn() {
+        saab.turnRight();
+        assertEquals(newDirection[1], saab.getDirection()[1], 0.01);
+    }
+
+    @Test
+    public void checkXDirectionAfterFullClockwiseRotation() {
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        assertEquals(start[0], saab.getDirection()[0], 0.01);
+    }
+
+    @Test
+    public void checkYDirectionAfterFullClockwiseRotation() {
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        assertEquals(start[1], saab.getDirection()[1], 0.01);
+    }
+
+    @Test
+    public void checkXDirectionAfterFullClockwiseRotationPlusOneTurn() {
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        assertEquals(newDirection[0], saab.getDirection()[0], 0.01);
+    }
+
+    @Test
+    public void checkYDirectionAfterFullClockwiseRotationPlusOneTurn() {
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        assertEquals(newDirection[1], saab.getDirection()[1], 0.01);
     }
 }
