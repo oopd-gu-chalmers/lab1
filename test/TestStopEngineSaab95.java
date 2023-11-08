77a1c0cc8da47d046b01ceb122b0e307a1736430
Gåsen <linda.bergevald@gmail.com>
2023-11-08 15:24:56 +0100

    Some more changes

--- test/TestStopEngineSaab95.java
+++ test/TestStopEngineSaab95.java
@@ -1,14 +1,15 @@
 package test;
 import org.junit.*;
 import src.Saab95;
-import src.Car;
+//import src.Car;
+//import static org.junit.Assert.assertTrue;
 
 public class TestStopEngineSaab95 {
     @Test
     public void StopEngineSaab95(){
         Saab95 Saab = new Saab95();
         Saab.stopEngine();
-        Assert.assertEquals(0, Saab.currentSpeed());
+        Assert.assertEquals(0, (int)Saab.getCurrentSpeed());
     }
     
 }
