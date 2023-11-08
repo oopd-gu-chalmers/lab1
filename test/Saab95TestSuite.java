18d51c9750d956121afaba561fce969d535fe814
Evelina Frännhag <evelinafrannhag@gmail.com>
2023-11-08 21:02:42 +0100

    Finish Saab tests

--- test/Saab95TestSuite.java
+++ test/Saab95TestSuite.java
@@ -1,8 +1,7 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-/*
+
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestSaab95DecrementSpeed.class, TestSaab95IncrementSpeed.class})
+@Suite.SuiteClasses({})
 public class Saab95TestSuite {
 }
- */
