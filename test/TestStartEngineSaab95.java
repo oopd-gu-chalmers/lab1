5f4b2447e7df368d4cc342ec966a402bb87f601c
Zeldiaa <ebba.a.munck@gmail.com>
2023-11-08 11:24:28 +0100

    update

---
+++ test/TestStartEngineSaab95.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.assertTrue;
+
+public class TestStartEngineSaab95 {
+    private Saab95 saab;
+
+    @Test
+    public void test(){
+        car.gas(-1);
+
+    }
+}
