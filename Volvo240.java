6ee6c0385f73856efa5f3d76a38f9ce4263f0ef7
GitoPot <102609722+GitoPot@users.noreply.github.com>
2023-11-09 08:24:07 +0100

    tag bort onödigheter

--- Volvo240.java
+++ Volvo240.java
@@ -1,28 +1,25 @@
 import java.awt.*;
 
-public class Volvo240 extends Cars{
+public class Volvo240 extends Cars {
 
     private final static double trimFactor = 1.25;
 
-    
-    public Volvo240(){
+
+    public Volvo240() {
         super(4, 100, Color.black, "Volvo240");
     }
-    
-    private double speedFactor(){
+
+    private double speedFactor() {
         return enginePower * 0.01 * trimFactor;
     }
 
-    private void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+    private void incrementSpeed(double amount) {
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
     }
 
-    private void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+    private void decrementSpeed(double amount) {
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
     }
+}
 
-    // TODO fix this method according to lab pm
-
-
-    // TODO fix this method according to lab pm
 
