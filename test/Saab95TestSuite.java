309604f9f19f1c4fbcacffa2459322c95a2c127c
Zeldiaa <ebba.a.munck@gmail.com>
2023-11-09 15:08:58 +0100

    Made Saab95 gas test

--- test/Saab95TestSuite.java
+++ test/Saab95TestSuite.java
@@ -2,6 +2,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestSaab95SpeedFactor.class,TestSaab95StartEngine.class})
+@Suite.SuiteClasses({TestSaab95Gas.class,TestSaab95SpeedFactor.class})
+
+
 public class Saab95TestSuite {
 }
