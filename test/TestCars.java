d87406bbb6252a4c391152c1d81d6314a1906ce9
Ponky <77840376+BaguetteConnoisseur@users.noreply.github.com>
2023-11-09 08:23:48 +0100

    Demosuite

--- test/TestCars.java
+++ test/TestCars.java
@@ -5,30 +5,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TestCars {
-    private Cars set;
     @Before
     public void init() {
-        set = new ARSet();
-        set.add(7);
-        set.add(7);
+        Saab95 mySaab95 = new Saab95();
+        Volvo240 myVolvo240 = new Volvo240();
     }
 
     @Test
-    public void testAddcheckifAdded() {
-        ARSet set = new ARSet();
-        int size = set.size();
-
-        set.add(5);
-        assertTrue(size < set.size());
-    }
-    @Test
-    public void testAddTwoIdenticalElementinset() {
-        assertEquals(1, set.size());
-    }
-
-    @Test
-    public void testAddTwoIdenticalElementinsetContainsAddedNum() {
-        assertTrue(set.contains(7));
-    }
-}
+    public void testif
 }
