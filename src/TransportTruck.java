71a845a6a5aa401ee23e99a82090eed97281cc95
Ponky <77840376+BaguetteConnoisseur@users.noreply.github.com>
2023-11-13 13:36:18 +0100

    Code by Ponky

--- src/TransportTruck.java
+++ src/TransportTruck.java
@@ -2,4 +2,18 @@ import java.awt.*;
 
 public class TransportTruck extends Scania { // WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 
+
+
+    @Override
+    public void raiseBedAngle() {
+        if (getCurrentSpeed() == 0) {
+            getBedAngle() = 70;
+        }
+    }
+    @Override
+    public void lowerBedAngle() {
+        if (getCurrentSpeed() == 0) {
+            bedAngle = 0;
+        }
+    }
 }
