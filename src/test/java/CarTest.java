97a92a2149203381dec6fa0d5d9c4685e519586d
Aron Larsson <arlars03@gmail.com>
2023-11-14 19:21:37 +0100

    Replace qualified name with import

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,33 +1,35 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
     Car car = new Car();
-    @org.junit.jupiter.api.Test
+    @Test
     void setColor() {
         car.setColor(Color.BLUE);
         assertEquals(Color.BLUE, car.getColor());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void startEngine() {
         car.startEngine();
         assertTrue(car.engineIsRunning());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void stopEngine() {
         car.stopEngine();
         assertFalse(car.engineIsRunning());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void gas() {
         double initialSpeed = car.getCurrentSpeed();
         car.gas(1);
         assertFalse(initialSpeed > car.getCurrentSpeed());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void brake() {
     }
 }
