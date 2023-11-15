5382781d005632cd0a6427f446c68bcf41c847ec
Zeldiaa <ebba.a.munck@gmail.com>
2023-11-15 09:38:54 +0100

    Made Workshop i think

--- src/CarWorkshop.java
+++ src/CarWorkshop.java
@@ -1,4 +1,13 @@
+import java.util.Vector;
 public class CarWorkshop {
-    private CarStack cars;
+    private Vector cars;
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void removeCar(Car car) {
+        cars.remove(car);
+    }
 
 }
