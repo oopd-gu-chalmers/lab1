2db11fe7033908b403c5adc7290052cb6666f61a
Affekaffe <mattias.enell10@gmail.com>
2023-11-16 13:43:14 +0100

    documentation

--- README.md
+++
@@ -1,4 +0,0 @@
-# One of the greates projects ever
-## By: Alfons Enelll, Anton Skoglund, Zuzanna Tarkowska 
-
-We have concured the first LAB and began work on the second.
--- src/assets/elements/vehicles/engines/TrimmedEngine.java
+++ src/assets/elements/vehicles/engines/TrimmedEngine.java
@@ -1,7 +1,18 @@
 package assets.elements.vehicles.engines;
 
+/**
+ * A TrimmedEngine is an Engine with a trim factor.
+ * The trim factor is multiplied to the engine power before generating power.
+ */
 public class TrimmedEngine extends Engine {
     private final double trimFactor;
+
+    /**
+     *
+     * @param enginePower the maximum power of the engine
+     * @param trimFactor the factor which is multiplied to the generated power
+     * @param weight the weight of the engine
+     */
     public TrimmedEngine(double enginePower, double trimFactor, double weight){
         super(enginePower, weight);
         this.trimFactor = trimFactor;
