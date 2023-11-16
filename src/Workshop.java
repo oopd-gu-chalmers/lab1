fd5de4cab0d4c43138e5067b7efdd22c357f997a
Zeldiaa <ebba.a.munck@gmail.com>
2023-11-16 09:01:53 +0100

    Worked on workshop

--- src/Workshop.java
+++ src/Workshop.java
@@ -3,14 +3,22 @@ import java.util.Vector;
 
 
 public class Workshop {
-    private Vector cars;
+    private final int maxCars;
+    private final String modelName;
+    private Vector<Car> cars = new Vector<>();
+
+    public Workshop(String modelName, int maxCars) {
+        this.modelName = modelName;
+        this.maxCars = maxCars;
+    }
 
     public void addCar(Car car) {
-        cars.add(car);
+        if (car.modelName == this.modelName && cars.size() < maxCars) {
+            cars.add(car);
+        }
     }
 
     public void removeCar(Car car) {
         cars.remove(car);
     }
-
 }
