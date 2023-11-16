e589c3e837578381243bd3c12a279c048f78afd0
Aron Larsson <arlars03@gmail.com>
2023-11-16 12:47:04 +0100

    Move attributes to Car class

--- src/main/java/Saab95.java
+++ src/main/java/Saab95.java
@@ -3,12 +3,7 @@ import java.awt.*;
 public class Saab95 implements ICar {
     private Car parent;
     public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    
+
     public Saab95(){
         nrDoors = 2;
         color = Color.red;
--- src/main/java/Volvo240.java
+++ src/main/java/Volvo240.java
@@ -3,12 +3,7 @@ import java.awt.*;
 public class Volvo240 implements ICar {
     private Car parent;
     public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    
+
     public Volvo240(){
         nrDoors = 4;
         color = Color.black;
