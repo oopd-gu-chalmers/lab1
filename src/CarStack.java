bffd0ec18dcaab7da5e7a553a4a468c488ed671a
Evelina Frännhag <evelinafrannhag@gmail.com>
2023-11-17 13:24:55 +0100

    Testing

--- src/CarStack.java
+++ src/CarStack.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 public class CarStack implements Iterable<Car>, Movable {
     private final int maxCars;
     private final int maxDoors;
-    private Stack<Car> cars;
+    private Stack<Car> cars = new Stack<Car>();
 
     public CarStack(int maxCars, int maxDoors){
         this.maxCars = maxCars;
@@ -12,7 +12,7 @@ public class CarStack implements Iterable<Car>, Movable {
     }
 
     public void addCar(Car car) {
-        if((cars.size() < maxCars) && car.isLoadable && (car.getNrDoors() <= maxDoors)) {
+        if((cars.size() < maxCars) && car.isLoadable && (car.getNrDoors() <= maxDoors) && !cars.contains(car)) {
             cars.push(car);
         }
     }
@@ -20,6 +20,14 @@ public class CarStack implements Iterable<Car>, Movable {
     public Car removeCar() {
         if (!cars.isEmpty()) return (Car) cars.pop(); //Säger att (Car) är redundant
         return null;
+    }
+
+    public int getNrOfLoadedCars() {
+        return cars.size();
+    }
+
+    public boolean isCarLoaded(Car car) {
+        return cars.contains(car);
     }
 
     public Iterator<Car> iterator() {
