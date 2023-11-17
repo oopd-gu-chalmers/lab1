48a3477999732f563c5bcd2f589b920e86fb15f2
Zeldiaa <ebba.a.munck@gmail.com>
2023-11-17 13:27:23 +0100

    Updated workshop more

--- src/Workshop.java
+++ src/Workshop.java
@@ -1,52 +1,21 @@
-import java.util.Objects;
 import java.util.Vector;
 
 
 
 public class Workshop<C> {
     private final int maxCars;
-    //private final String modelName;
     public Vector<C> cars = new Vector<>();
 
     public Workshop(int maxCars) {
         this.maxCars = maxCars;
-        //this.modelName = "Null";
     }
-
-    //public Workshop(String modelName, int maxCars) {
-        //this.modelName = modelName;
-    //    this.maxCars = maxCars;
-   // }
-
 
     public void addCar(C car) {
         if (cars.size() < maxCars)
             cars.add(car);
     }
 
-    /*
-    public void addCar(Volvo240 volvo) {
-        cars.add(volvo);
-    }
-
-    public void addCar(Saab95 saab) {
-        cars.add(saab);
-    }
-
-
-
-
-    public void addCar(Car car) {
-        if ((Objects.equals(car.modelName, this.modelName) || Objects.equals(this.modelName, "Null")) && cars.size() < maxCars)
-            cars.add(car);
-    }
-
-     */
-
     public void removeCar(C car) {
         cars.remove(car);
     }
-
-
-
 }
