de9ae18cca290e930dd4284fc1cc95be30cbf848
Aron Larsson <arlars03@gmail.com>
2023-11-20 12:45:27 +0100

    Create class for holding unit size constants

---
+++ src/main/java/UnitSize.java
@@ -0,0 +1,5 @@
+public class UnitSize {
+    public static final int SMALL_CAR_UNIT_SIZE = 10;
+    public static final int LARGE_CAR_UNIT_SIZE = 15;
+    public static final int TRUCK_UNIT_SIZE = 30;
+}
