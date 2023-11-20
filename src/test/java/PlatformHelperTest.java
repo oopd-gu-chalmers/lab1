526b585b7d51585d116f8ec00cdbdd320f7621a0
Aron Larsson <arlars03@gmail.com>
2023-11-20 12:43:57 +0100

    Create tests for PlatformHelper

---
+++ src/test/java/PlatformHelperTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlatformHelperTest {
+    PlatformHelper platformHelper = new PlatformHelper(0, 100);
+
+    @Test
+    void closePlatform() {
+        platformHelper.setPlatformAngle(10);
+        platformHelper.closePlatform();
+        assertTrue(platformHelper.platformIsFullyClosed());
+    }
+
+    @Test
+    void openPlatform() {
+        platformHelper.setPlatformAngle(10);
+        platformHelper.openPlatform();
+        assertTrue(platformHelper.platformIsFullyOpened());
+    }
+}
