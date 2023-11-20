311a4b62a47855a9fbea4a79bc68952256da3323
Aron Larsson <arlars03@gmail.com>
2023-11-20 11:59:52 +0100

    Add unitDirectionPoint to make additions of directions easier

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,11 +1,31 @@
+import java.awt.geom.Point2D;
+
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST;
+    NORTH(Math.PI / 2),
+    EAST(0),
+    SOUTH(3 * Math.PI / 2),
+    WEST(Math.PI);
 
     private static final Direction[] directions = values();
+    private final Point2D.Double unitDirectionPoint;
+
+    Direction(Point2D.Double unitDirectionPoint) {
+        this.unitDirectionPoint = unitDirectionPoint;
+    }
+
+    Direction(double angle) {
+        this(new Point2D.Double(Math.cos(angle), Math.sin(angle)));
+    }
+
     public Direction getNext() {
         return directions[(this.ordinal() + 1) % directions.length];
     }
+
     public Direction getPrevious() {
         return directions[(this.ordinal() - 1) % directions.length];
+    }
+
+    public Point2D.Double getUnitDirectionPoint() {
+        return unitDirectionPoint;
     }
 }
