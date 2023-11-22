9967b2ece8bbaab6039a772c89bc7a355c7e2e5e
Aron Larsson <arlars03@gmail.com>
2023-11-22 14:51:09 +0100

    Add generic type parameter T extends Loadable to CanLoad interface

--- src/main/java/CanLoad.java
+++ src/main/java/CanLoad.java
@@ -1,12 +1,12 @@
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
-public interface CanLoad {
-    ArrayList<Loadable> getLoadedItems();
+public interface CanLoad<T extends Loadable> {
+    ArrayList<T> getLoadedItems();
     int getMaxNrOfItems();
     int getNrOfAvailableSlots();
     int getMaximumUnitSize();
-    boolean load(Loadable item); // Return true if item could be loaded, else false.
-    Loadable unloadLast();
+    boolean load(T item); // Return true if item could be loaded, else false.
+    T unload();
     Point2D.Double getUnloadingPosition();
 }
