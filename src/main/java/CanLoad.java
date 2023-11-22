6253d25b33fe08c7e84e0a6ad91308f9736453ae
Aron Larsson <arlars03@gmail.com>
2023-11-22 14:49:36 +0100

    Rename to reflect functionality change

--- src/main/java/CanLoad.java
+++ src/main/java/CanLoad.java
@@ -2,9 +2,9 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public interface CanLoad {
-    int getTotalCapacity();
-    int getAvailableCapacity();
     ArrayList<Loadable> getLoadedItems();
+    int getMaxNrOfItems();
+    int getNrOfAvailableSlots();
     int getMaximumUnitSize();
     boolean load(Loadable item); // Return true if item could be loaded, else false.
     Loadable unloadLast();
