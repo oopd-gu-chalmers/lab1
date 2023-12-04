8632cb5407e1e7fd4a3d51087e5f201d2edb30bb
Evelina Frännhag <evelinafrannhag@gmail.com>
2023-12-04 10:54:33 +0100

    Implement class Vehicle<T extends Movable & Engine>

--- src/Vehicle2.java
+++ src/Vehicle2.java
@@ -1,6 +1,27 @@
-public class Vehicle2<T extends Movable> {
-    public T v;
+public class Vehicle2<T extends Movable & Engine> {
+    public T vehicle;
     public Vehicle2(T vehicle) {
-        this.v = vehicle;
-        }
+        this.vehicle = vehicle;
+    }
+    public void move() {
+        vehicle.move();
+    }
+    public void turnLeft() {
+        vehicle.turnLeft();
+    }
+    public void turnRight() {
+        vehicle.turnRight();
+    }
+    public void startEngine() {
+        vehicle.startEngine();
+    }
+    public void stopEngine() {
+        vehicle.stopEngine();
+    }
+    public void gas() {
+        vehicle.gas();
+    }
+    public void brake() {
+        vehicle.brake();
+    }
 }
