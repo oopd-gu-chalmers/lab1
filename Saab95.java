1fc71abc9890e449286f3a259376fa06d17a6600
tedg2 <tedgr@chalmers.se>
2024-01-17 09:19:43 +0100

    add comment

--- Saab95.java
+++ Saab95.java
@@ -1,14 +1,13 @@
 import java.awt.*;
 
 public class Saab95{
-
     public boolean turboOn;
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
-    
+    //TESTCOMMENT TO BE REMOVED
     public Saab95(){
         nrDoors = 2;
         color = Color.red;
