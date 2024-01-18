303c77a00f4745dfa20424b0c230969d11fbdf96
AntonJohansson02 <76776791+AntonJohansson02@users.noreply.github.com>
2024-01-18 14:35:07 +0100

    Uppgift 1 klart

--- Saab95.java
+++ Saab95.java
@@ -2,16 +2,6 @@ import java.awt.*;
 
 public class Saab95 extends Car{
     public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    //TESTCOMMENT TO BE REMOVED
-    // Anothercomment to be removed 2324
-
-    //hello
-
 
     public Saab95(){
         nrDoors = 2;
@@ -22,33 +12,6 @@ public class Saab95 extends Car{
         stopEngine();
     }
     
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
-
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-
     public void setTurboOn(){
 	    turboOn = true;
     }
@@ -57,27 +20,11 @@ public class Saab95 extends Car{
 	    turboOn = false;
     }
     
+    @Override
     public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
-
-    public void incrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-    }
-    
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
-
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
-    }
 }
+
