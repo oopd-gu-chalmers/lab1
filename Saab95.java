0da8245a47b6c541af8ff76991a74853e6d7eae3
AntonJohansson02 <76776791+AntonJohansson02@users.noreply.github.com>
2024-01-18 13:10:47 +0100

    Small changes

--- Saab95.java
+++ Saab95.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Saab95{
+public class Saab95 extends Car{
     public boolean turboOn;
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
--- Volvo240.java
+++ Volvo240.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Car{
 
     public final static double trimFactor = 1.25;
     public int nrDoors; // Number of doors on the car
