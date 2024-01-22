08d4e92a0ada6cf992553cb4f1c5b616dbf200ec
ViktoriaAndersson <vianders@chalmers.se>
2024-01-22 16:39:23 +0100

    del2

--- Car.java
+++ Car.java
@@ -1,13 +1,28 @@
 import java.awt.*;
+import java.math.*;
 
-public class Car {
+public abstract class Car implements Movable {
     protected int nrDoors; // Number of doors on the car
     protected double enginePower; // Engine power of the car
     protected double currentSpeed; // The current speed of the car
     protected Color color; // Color of the car
     protected String modelName; // The car model name
+    protected double xpos;
+    protected double ypos;
+    protected double direction;
 
     //eventuellt skapa Konstruktor??
+    public Car() {
+        this.xpos = 0;
+        this.ypos = 0;
+        this.direction = 0;
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+        stopEngine();
+
+    }
 
     protected int getNrDoors() {
 
@@ -37,6 +52,21 @@ public class Car {
 
     protected void stopEngine(){
         currentSpeed = 0;
+    }
+
+    @Override
+    public void move() {
+        xpos += Math.cos(Math.toRadians(direction)) * currentSpeed;
+        ypos += Math.sin(Math.toRadians(direction)) * currentSpeed;
+    }
+    @Override
+    public void turnLeft() {
+        direction += 90;
+    }
+
+    @Override
+    public void turnRight() {
+        direction -= 90;
     }
     //ska speedfactor, inctement/decrement speed? vara med i och med att båda klasserna har det?
 
