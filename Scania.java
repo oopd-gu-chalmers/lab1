fc202888dc2674900fef85a8dd82fee91a795421
NoaSjostrand <noasj@chalmers.se>
2024-01-25 11:15:58 +0100

    yolo scania is best

--- Scania.java
+++ Scania.java
@@ -2,9 +2,40 @@ import java.awt.*;
 
 public class Scania extends Truck {
 
-    public Scania(int nrDoors, Color color, double enginePower, String modelname){
-        super(nrDoors, color, enginePower, modelname);
+    public Scania(int nrDoors, Color color, double enginePower, String modelName){
+        super(nrDoors, color, enginePower, modelName);
     }
 
+    double speedFactor() {
+        return this.getEnginePower()*0.005;
+    }
 
+    public void pivotUp(){
+        try{
+        double newAngle = get_platformAngle() + 10;
+        if (newAngle > 70){
+            throw new Exception("Platform too steep");
+        } else if (getCurrentSpeed() > 0) {
+            throw new Exception("Can't pivot while moving");
+        } else{
+            set_platformAngle(newAngle);
+        }
+    } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void pivotDown(){
+        try {
+        double newAngle = get_platformAngle() - 10;
+        if (newAngle < 0){
+            throw new Exception("Platform already flat");
+        }
+        else {
+            set_platformAngle(newAngle);
+        }
+    } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
