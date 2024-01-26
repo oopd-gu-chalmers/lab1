c0e060bc6424b4e8cde6282317ac7ee1fa96999d
William Körner <mejl@williamkorner.se>
2024-01-26 14:42:40 +0100

    Added add and remove methods to garage

--- Garage.java
+++ Garage.java
@@ -5,13 +5,16 @@ public class Garage {
     private boolean acceptsAllModels; //If the garage accepts all models
     private Car[] cars = new Car[capacity]; //Array of cars in the garage
 
+    public Garage(int capacity, String[] acceptedModels, boolean acceptsAllModels) {
+        this.capacity = capacity;
+        this.acceptedModels = acceptedModels;
+        this.acceptsAllModels = acceptsAllModels;
+    }
 
     public boolean checkCarAccepted(Car car) {
-        if (this.acceptsAllModels) {
-            return true;
-        }
+        if (acceptsAllModels) {return true;}
         else {
-            for (String model : this.acceptedModels) {
+            for (String model : acceptedModels) {
                 if (car.getModelName().equals(model)) {
                     return true;
                 }
@@ -20,5 +23,26 @@ public class Garage {
         }
     }
 
+    public void addCar(Car car) throws Exception {
+        if (checkCarAccepted(car)) {
+            for (int i = 0; i < cars.length; i++) {
+                if (cars[i] == null) {
+                    cars[i] = car;
+                    break;
+                }
+                else if (i == cars.length - 1) {throw new Exception("Garage is full");}
+            }
+        }
+        else {throw new Exception("Garage does not accept this model"); }
+    }
 
+    public void removeCar(Car car) throws Exception {
+        for (int i = 0; i < cars.length; i++) {
+            if (cars[i] == car) {
+                cars[i] = null;
+                break;
+            }
+            else if (i == cars.length - 1) {throw new Exception("Car not found");}
+        }
+    }
 }
