714293b6a6b0b2ab5457946eb4e11937ad3e924e
leozhu <leozhu00@hotmail.com>
2024-02-01 15:19:11 +0100

    Skapat test directory och skapat test fil för Scania

---
+++ tests/ScaniaTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScaniaTest {
+
+    Scania scania;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        scania = new Scania();
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFlatbedAngle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void raisePlatform() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lowerPlatform() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void startEngine() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void speedFactor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void incrementSpeed() {
+    }
+}
