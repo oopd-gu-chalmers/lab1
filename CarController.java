827029177088979a1bb875b012baaf4427c04ea8
felixtan <felix3123@hotmail.com>
2024-02-12 15:04:57 +0100

    test3

--- CarController.java
+++ CarController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 * modifying the model state and the updating the view.
  */
 
-public class CarController  {
+public class CarController {
     // member fields:
 
     // The delay (ms) corresponds to 20 updates a sec (hz)
@@ -22,35 +22,55 @@ public class CarController  {
 
     CarView frame;
     // A list of cars, modify if needed
-     ArrayList<Car> cars = new ArrayList<>();
+    ArrayList<Car> cars = new ArrayList<>();
 
     //methods:
 
     public static void main(String[] args) {
         // Instance of this class
         CarController cc = new CarController();
+        Car v0 = new Volvo240();
+        cc.cars.add(v0);
 
-         cc.cars.add(new Volvo240());
+        Car s0 = new Saab95();
+        s0.setxPos(0);
+        s0.setyPos(200);
+        cc.cars.add(s0);
+
+        Car sc0 = new Scania();
+        sc0.setxPos(0);
+        sc0.setyPos(400);
+        cc.cars.add(sc0);
+
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
+
 
         // Start the timer
         cc.timer.start();
     }
 
     /* Each step the TimerListener moves all the cars in the list and tells the
-    * view to update its images. Change this method to your needs.
-    * */
+     * view to update its images. Change this method to your needs.
+     * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             for (Car car : cars) {
                 car.move();
                 int x = (int) Math.round(car.getxPos());
                 int y = (int) Math.round(car.getyPos());
-                frame.drawPanel.moveit(x, y);
-                // repaint() calls the paintComponent method of the panel
-                frame.drawPanel.repaint();
+                frame.drawPanel.addCar(car);
+                frame.drawPanel.moveit(car, x, y);
+                System.out.println(car.getxPos());
+                if (x > frame.drawPanel.getWidth() - 100) {
+                    car.setDirection(3);
+                }
+                if (x < 0) {
+                    car.setDirection(1);
+                } else
+                    // repaint() calls the paintComponent method of the panel
+                    frame.drawPanel.repaint();
             }
         }
     }
@@ -58,9 +78,46 @@ public class CarController  {
     // Calls the gas method for each car once
     void gas(int amount) {
         double gas = ((double) amount) / 100;
-        for (Car car : cars
-                ) {
+        for (Car car : cars) {
             car.gas(gas);
+        }
+    }
+
+    void brake(int amount) {
+        double brake = ((double) amount) / 100;
+        for (Car car : cars) {
+            car.brake(brake);
+        }
+    }
+
+    void turboOn() {
+        for (Car c : cars) {
+            if (c instanceof Saab95) {
+                ((Saab95) c).setTurboOn();
+            }
+        }
+    }
+
+    void turboOff() {
+        for (Car c : cars) {
+            if (c instanceof Saab95) {
+                ((Saab95) c).setTurboOff();
+            }
+        }
+    }
+
+    void raiseFlak() {
+        for(Car c : cars) {
+            if (c instanceof Scania) {
+                ((Scania) c).raiseRamp();
+            }
+        }
+    }
+    void lowerFlak() {
+        for(Car c : cars) {
+            if (c instanceof Scania) {
+                ((Scania) c).lowerRamp();
+            }
         }
     }
 }
